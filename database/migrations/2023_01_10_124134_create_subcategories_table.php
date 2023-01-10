848182e75e5db581b192a2d2098cdd4967f16e77
antoHero <veeqanto@gmail.com>
2023-01-10 13:45:59 +0100

    Setup breeze and create more models and migrations

---
+++ database/migrations/2023_01_10_124134_create_subcategories_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Category;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subcategories', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Category::class);
+            $table->string('name')->nullable(false);
+            $table->string('slug')->nullable(false);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subcategories');
+    }
+};
